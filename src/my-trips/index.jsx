747e40307a706bf94b7339e1a86592a8d3742ec0
tabish khan <tabishkhanvaliant7861234@gmail.com>
2025-05-22 02:45:49 -0500

    chjhsg
    Signed-off-by: tabish khan <tabishkhanvaliant7861234@gmail.com>

--- src/my-trips/index.jsx
+++ src/my-trips/index.jsx
@@ -31,34 +31,34 @@ function MyTrips() {
     }
 
     return (
-        // <div className='sm:px-10 md:px-32 lg:px-56 px-5  mt-10'>
-        //     <h2 className='font-bold mt-20 text-center text-3xl'>My Trips</h2>
-        //     <div className='grid grid-cols-2 mt-10 md:grid-cols-3 gap-5'>
-        //         {userTrips?.length>0?userTrips.map((trip, index)=>(
-        //             <UserTripCardItem trip={trip} key={index} />
-        //         )):[1,2,3,4,5,6].map((item, index)=>(
-        //             <div key={index} className='h-[220px] w-full bg-slate-200 animate-pulse rounded-xl'>
+        <div className='sm:px-10 md:px-32 lg:px-56 px-5  mt-10'>
+            <h2 className='font-bold mt-20 text-center text-3xl'>My Trips</h2>
+            <div className='grid grid-cols-2 mt-10 md:grid-cols-3 gap-5'>
+                {userTrips?.length>0?userTrips.map((trip, index)=>(
+                    <UserTripCardItem trip={trip} key={index} />
+                )):[1,2,3,4,5,6].map((item, index)=>(
+                    <div key={index} className='h-[220px] w-full bg-slate-200 animate-pulse rounded-xl'>
 
-        //             </div>
-        //         ))}
-        //     </div>
-        // </div>
-        <div className='sm:px-10 md:px-32 lg:px-56 px-5 mt-10'>
-  <h2 className='font-bold mt-20 text-center text-3xl text-black'>My Trips 🛣</h2>
+                    </div>
+                ))}
+            </div>
+        </div>
+  //       <div className='sm:px-10 md:px-32 lg:px-56 px-5 mt-10'>
+  // <h2 className='font-bold mt-20 text-center text-3xl text-black'>My Trips 🛣</h2>
 
-  <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-10 '>
-    {userTrips?.length > 0
-      ? userTrips.map((trip, index) => (
-          <UserTripCardItem trip={trip} key={index} />
-        ))
-      : [1, 2, 3, 4, 5, 6].map((item, index) => (
-          <div
-            key={index}
-            className='h-[220px] w-full bg-slate-200/20 animate-pulse rounded-xl'
-          />
-        ))}
-  </div>
-</div>
+  // <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-10 '>
+  //   {userTrips?.length > 0
+  //     ? userTrips.map((trip, index) => (
+  //         <UserTripCardItem trip={trip} key={index} />
+  //       ))
+  //     : [1, 2, 3, 4, 5, 6].map((item, index) => (
+  //         <div
+  //           key={index}
+  //           className='h-[220px] w-full bg-slate-200/20 animate-pulse rounded-xl'
+  //         />
+  //       ))}
+//   </div>
+// </div>
 
     )
 }
