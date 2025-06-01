992511bd826a4a6381e864aead1941e10ec1a615
tabish khan <tabishkhanvaliant7861234@gmail.com>
2025-06-01 03:34:17 -0500

    wsw
    Signed-off-by: tabish khan <tabishkhanvaliant7861234@gmail.com>

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -14,7 +14,7 @@ function Hero() {
         playsInline
         className="absolute top-0 left-0 w-full h-full object-cover z-0"
       >
-        <source src="../../../public/hero.mp4" type="video/mp4" />
+        <source src="../../../hero.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 
